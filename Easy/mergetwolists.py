57d56ab00c70ba8d8783f01f50fa430fd6120f87
Zeid Al-Ameedi <zeid.al-ameedi@wsu.edu>
2019-11-01 23:39:57 -0700

    Day 17, another long break cuz exams

---
+++ Easy/mergetwolists.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 != None and l2 == None:
+            return l1
+        if l1 == None and l2 != None:
+            return l2
+        if l1 == None and l2 == None:
+            return None
+        
+        if l1.val <= l2.val:
+            l3 = ListNode(l1.val)
+            l3.next = self.mergeTwoLists(l1.next, l2)
+        else:
+            l3 = ListNode(l2.val)
+            l3.next = self.mergeTwoLists(l1, l2.next)
+        return l3
+
+        
